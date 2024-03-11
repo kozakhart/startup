7b4a81b16152423999a94e69537d90d20e6f7197
Peter Hart <kozakhart@gmail.com>
2024-03-10 19:33:50 -0600

    add middleware

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require('express');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
