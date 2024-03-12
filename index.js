356f5001c7791b3f879cb5c38b8b32f78d456ed6
Peter Hart <kozakhart@gmail.com>
2024-03-11 19:03:17 -0600

    add api

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
