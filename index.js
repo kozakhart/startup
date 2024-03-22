5519277d2660f992f436e1090eadd644845a8531
Peter Hart <kozakhart@gmail.com>
2024-03-21 19:15:35 -0600

    revamp index, works with get and post curls

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ const express = require('express');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
-
-// middleware
-app.use(express.static('public'));
 app.use(express.json());
+app.use(express.static('public'));
 
-
-app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
-  
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-  
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+let orders = [];
+
 apiRouter.get('/orders', (_req, res) => {
-    res.send(scores);
+    res.json(orders);
   });
   
 apiRouter.post('/orders', (req, res) => {
-    scores = updateOrders(req.body, scores);
-    res.send(scores);
-  });
+    const newOrder = req.body;
+    newOrder.timestamp = new Date().getTime();
+
+    orders.push(newOrder);
+
+    res.json(orders);
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
